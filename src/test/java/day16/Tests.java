7bb7409848f61e4533e173caaf53a312226c8b74
Jeremy Hunt <huntingfrogs@hotmail.com>
2022-12-04 22:06:16 +1300

    Skip not yet implemented tests in templates

--- src/test/java/day16/Tests.java
+++ src/test/java/day16/Tests.java
@@ -4,18 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.InputStream;
 
-import org.junit.Test;
-
 public class Tests {
-    @Test
+
     public void part_1_example_1() throws Exception {
         try (InputStream input = Tests.class.getResourceAsStream("example1.txt")) {
             Solution solution = new SolutionImpl();
             assertEquals((Object) null, solution.solvePart1(input));
         }
     }
-    
-    @Test
+
     public void part_1() throws Exception {
         try (InputStream input = Tests.class.getResourceAsStream("input.txt")) {
             Solution solution = new SolutionImpl();
@@ -23,15 +20,13 @@ public class Tests {
         }
     }
 
-    @Test
     public void part_2_example_1() throws Exception {
         try (InputStream input = Tests.class.getResourceAsStream("example1.txt")) {
             Solution solution = new SolutionImpl();
             assertEquals((Object) null, solution.solvePart2(input));
         }
     }
-    
-    @Test
+
     public void part_2() throws Exception {
         try (InputStream input = Tests.class.getResourceAsStream("input.txt")) {
             Solution solution = new SolutionImpl();
